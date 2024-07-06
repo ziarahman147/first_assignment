ad33a13cdfc6934f231dc5e7bbdd215f06ff39e9
ziarahman147 <ziarahman147@gmail.com>
2024-07-06 22:22:56 +0600

    a new key-value pair has been added

--- Dictionaries.py
+++ Dictionaries.py
@@ -5,6 +5,7 @@ book = {
 }
 print(f'Values of the book = {list(book.values())}')
 book['genre'] = 'Psychological Horror'
+book['country'] = 'USA'
 print('The Entire Updated Book is printed below:')
 for i in book.items():
   print(i)
