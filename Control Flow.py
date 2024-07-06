ab04bff50537658b2750f22e075c60f702a369ab
ziarahman147 <ziarahman147@gmail.com>
2024-07-06 22:55:23 +0600

    Remaining Files have been copied

---
+++ Control Flow.py
@@ -0,0 +1,5 @@
+num = (int)(input("Enter An Integer: "))
+if (num % 2 == 0):
+  print("You've entered an EVEN number")
+else:
+  print("You've entered an ODD number")
---
+++ Data Types and Variables.py
@@ -0,0 +1,20 @@
+#Variable Declaration:
+int_a =  34
+int_b = 39
+
+float_c = 34.3187
+string_d = "First Assignment of This course"
+boolean_e = True
+#Data Type Showcase:
+print(type(int_a))
+print(type(int_b))
+print(type(float_c))
+print(type(string_d))
+print(type(boolean_e))
+#Arithmetic Operations:
+print("a + b =",(str)(int_a + int_b) )
+print("a - b =",(str)(int_a - int_b) )
+print("a / b =",(str)((float(int_a) / int_b)))
+print("b / c =",(str)((int_b / float_c)))
+print("c * b =",(str)(float_c * int_b) )
+print("b % a =",(str)(int_b % int_a) )
